23636336b95b150941254330420759b861fb70da
Galang Putratama <galang@detik.com>
2025-07-14 11:35:17 +0700

    setup github pages lagi

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './view/Home/index'
 import Pages from './view/Pages/index'
 import './App.css'
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/page/:page" element={<Pages />} />
         <Route path="*" element={<Home />} /> {/* fallback */}
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
