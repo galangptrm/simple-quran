87267b9afe562dedcbdaaae77035ffa6a2627bb9
Galang Putratama <galang@detik.com>
2025-07-14 10:47:57 +0700

    update vite config & hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from './view/Home/index'
 import Pages from './view/Pages/index'
 import './App.css'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/page/:page" element={<Pages />} />
         <Route path="*" element={<Home />} /> {/* fallback */}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
