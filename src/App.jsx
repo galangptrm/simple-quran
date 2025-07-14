8e25b265286291d09b623e045c2f7061394ec9bf
Galang Putratama <galang@detik.com>
2025-07-14 11:48:12 +0700

    change router to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from './view/Home/index'
 import Pages from './view/Pages/index'
 import './App.css'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/page/:page" element={<Pages />} />
         <Route path="*" element={<Home />} /> {/* fallback */}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
